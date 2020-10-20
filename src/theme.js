8dee9598657b9977539709112e28d7ee069d8fa7
FAISAL JILLANI SATTI <72972379+FJSofficial@users.noreply.github.com>
2020-10-20 15:50:34 +0500

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,36 @@
+import {createMuiTheme} from "@material-ui/core/styles";
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#eee',
+        },
+        text:{
+            primary:'#253858',
+            secondary:'#091E42'
+        }
+    },
+    overrides: {
+        MUIRichTextEditor: {
+            editor: {
+                maxHeight: '500px',
+                wordBreak: 'break-all',
+                overflowY: 'auto',
+                borderBottom: "1px solid lightgrey"
+            }
+        }
+    }
+});
+
+export default theme;
